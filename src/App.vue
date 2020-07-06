ed3f1df3938015d926ca349e60c995dda56279f6
mdtk-git <m.u.p.s.4.15.17@gmail.com>
2020-07-07 07:55:09 +0900

    component, propsにて親から子へのデータ受け渡し

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <template>
   <div>
     <LikeHeader></LikeHeader>
-    <LikeNumber></LikeNumber> <!-- コンポーネントの呼び出し !-->
+    <h2>{{ number }}</h2>
+    <LikeNumber :total-number="number"></LikeNumber> <!-- コンポーネントの呼び出し !-->
+    <LikeNumber :total-number="number"></LikeNumber>
   </div>
 </template>
 
@@ -9,6 +11,11 @@
 import LikeHeader from "./components/LikeHeader.vue"; // コンポーネントのローカル登録
 
 export default {
+  data() {
+    return {
+      number: 10
+    }
+  },
   components: {
     LikeHeader
   }
