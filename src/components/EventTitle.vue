cfbd298e4bce99ece318020c7e6c3dfc0d5f7bf1
mdtk-git <m.u.p.s.4.15.17@gmail.com>
2020-07-13 15:30:41 +0900

    v-modelでフォーム作成および修飾子の使い方。

---
+++ src/components/EventTitle.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <label for="title"></label>
+    <input
+      id="title"
+      type="text"
+      :value="eventData.title"
+      @input="eventData.title = $event.target.value"
+    >
+    <pre>{{ eventData.title }}</pre>
+  </div>
+</template>
