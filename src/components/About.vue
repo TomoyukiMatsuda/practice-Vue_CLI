320abe316a4bfacb33c2745103b9a876d904097d
mdtk-git <m.u.p.s.4.15.17@gmail.com>
2020-07-18 09:23:40 +0900

    transition animation を学習

--- src/components/About.vue
+++
@@ -1,6 +0,0 @@
-<template>
-  <div>
-    <p>Aboutだよ</p>
-    <input type="text">
-  </div>
-</template>
--- src/main.js
+++ src/main.js
@@ -1,17 +1,5 @@
 import Vue from 'vue';
 import App from './App.vue';
-import LikeNumber from "./components/LikeNumber.vue";
-
-Vue.config.productionTip = false
-Vue.component("LikeNumber", LikeNumber); // コンポーネントのグローバル登録
-Vue.filter("upCase", function(val) {
-  return val.toUpperCase();
-});
-Vue.mixin({
-  created() {
-    console.log("グローバルミックスいん");
-  }
-});
 
 new Vue({
   render: h => h(App),
