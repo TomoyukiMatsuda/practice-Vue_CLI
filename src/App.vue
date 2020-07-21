579ec00a4404ec121119c03268655c47c81bbf18
mdtk-git <m.u.p.s.4.15.17@gmail.com>
2020-07-21 18:41:46 +0900

    transition-group javascriptフックの学習

--- src/App.vue
+++ src/App.vue
@@ -11,7 +11,38 @@
     <button @click="myAnimation = 'slide'">スライド</button>
     <button @click="myAnimation = 'fade'">フェイド</button>
     <p>{{ myAnimation }}</p>
+
+    <br>
+    <button @click="add">追加</button>
+    <ul style="width: 200px; margin: auto;">
+      <transition-group>
+        <li
+          style="cusor: pointer;"
+          v-for="(number, index) in numbers"
+          @click="remove(index)"
+          key="number"
+          >{{ number }}
+        </li>
+      </transition-group>
+    </ul>
+    <br>
     <button @click="show = !show">切り替え</button>
+    <br><br>
+    <transition
+      :css="false"
+      @before-enter="beforeEnter"
+      @enter="enter"
+      @leave="leave"
+    >
+      <div class="circle" v-if="show"></div>
+    </transition>
+
+    <br>
+    <button @click="myComponent = 'ComponentA'">コンポーネントA</button>
+    <button @click="myComponent = 'ComponentB'">コンポーネントB</button>
+    <transition name="fade" mode="out-in">
+      <component :is="myComponent"></component>
+    </transition>
     <transition
       name="fade"
       mode="out-in"
@@ -34,6 +65,8 @@
 
 <script>
 import Childe from "./components/Childe.vue";
+import ComponentA from "./components/ComponentA";
+import ComponentB from "./components/ComponentB";
 
 export default {
   data() {
@@ -41,15 +74,65 @@ export default {
       show: true,
       myAnimation: 'slide',
       data: "データ",
+      myComponent: "ComponentA",
+      numbers: [0, 1, 2],
+      nextNumber: 3,
     }
   },
   components: {
     Childe,
+    ComponentA,
+    ComponentB,
+  },
+  methods: {
+    randomIndex() {
+      return Math.floor(Math.random() * this.numbers.length);
+    },
+    add() {
+      this.numbers.splice(this.randomIndex(), 0, this.nextNumber);
+      this.nextNumber += 1;
+    },
+    remove(index) {
+      this.numbers.splice(index, 1);
+    },
+    beforeEnter(el) {
+      el.style.transform = "scale(0)"
+    },
+    enter(el, done) {
+      let scale = 0;
+      const interval = setInterval(() => {
+        el.style.transform = `scale(${scale})`;
+        scale += 0.1;
+        if (scale > 1) {
+          clearInterval(interval);
+          done();
+        }
+      }, 40);
+    },
+    leave(el, done) {
+      let scale = 1;
+      const interval = setInterval(() => {
+        el.style.transform = `scale(${scale})`;
+        scale -= 0.1;
+        if (scale < 0 ) {
+          clearInterval(interval);
+          done();
+        }
+      }, 40);
+    }
   }
 }
 </script>
 
 <style scoped>
+.circle {
+  width: 200px;
+  height: 200px;
+  margin: auto;
+  border-radius: 100px;
+  background-color: deeppink;
+}
+
 .fade-enter {
   opacity: 0;
 }
