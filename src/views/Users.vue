6dbe4b524072b669a866bac7debe045a337cc2cd
mdtk-git <m.u.p.s.4.15.17@gmail.com>
2020-07-25 16:12:47 +0900

    query の設定について学習

--- src/views/Users.vue
+++ src/views/Users.vue
@@ -5,8 +5,8 @@
     <router-link to="/users/2">ユーザー２</router-link>
     <hr>
     <h3>User No.{{ id }}</h3>
-    <router-link :to="'/users/' + (Number(id) + 1) + '/profile'">次のユーザー</router-link>
-    <router-link :to="{ name: 'users-id-profile', params: { id: Number(id) + 1 } }">次のユーザー</router-link>
+    <router-link :to="'/users/' + (Number(id) + 1) + '/profile?lang=ja'">次のユーザー</router-link>
+    <router-link :to="{ name: 'users-id-profile', params: { id: Number(id) + 1 }, query: { lang: 'ja', page: 2} }">次のユーザー</router-link>
     <router-view></router-view> <!-- chldren で登録したpathコンポーネントが入る。 -->
   </div>
 </template>
