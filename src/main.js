a60bcd99625090b5adec407690787f858cf09deb
mdtk-git <m.u.p.s.4.15.17@gmail.com>
2020-07-29 20:56:29 +0900

    beforeEachについて学習

--- src/main.js
+++ src/main.js
@@ -2,6 +2,14 @@ import Vue from 'vue';
 import App from './App.vue';
 import router from "./router";
 
+router.beforeEach((to, from, next) => { // ページ遷移する時に発火する処理
+  console.log('グローバルビフォーイーチ');
+  if (to.path === '/users/1') {
+    next( {path: '/' });
+  }
+  next();
+});
+
 new Vue({
   router,
   render: h => h(App),
